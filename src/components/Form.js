3759000f90a5ee396b39c669b9691c1232cf1889
vmirella <vmirella_17@hotmail.com>
2018-10-15 12:02:01 -0500

    Añadiendo productos a lista y calculando total

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,34 @@
 import React from 'react';
 
-
 const Form = (props) => {
 
   return (
     <div className = "row">
       <div className = "col s10">
-        <div>
-          <ul className="collection with-header">
-            <li className="collection-header"><h4>Pedido</h4></li>
-            <li className="collection-item">Producto S/. 12</li>
-            <li className="collection-item">Producto S/. 12</li>
-            <li className="collection-item">Producto S/. 12</li>
-            <li className="collection-item">Producto S/. 12</li>
-          </ul>
-        </div>
-        <h4>Total S/. 200.50</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>Cantidad</th>              
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.arrayOrders.map((order, i) => {
+              return (
+                <tr key={i}>
+                  <td>{order.name}</td>
+                  <td>S/. {order.price}</td>
+                  <td>{order.quantity}</td>                  
+                  <td>S/. {order.subtotal}</td>
+                </tr>
+              )
+            })}            
+          </tbody>
+        </table>
+
+        <h4>Total S/. {props.total}</h4>
       </div>
     </div>
   )
--- src/container/App.js
+++ src/container/App.js
@@ -13,10 +13,35 @@ class App extends Component {
       orders: [],
       total: 0
     }
-    
+    this.addProduct = this.addProduct.bind(this);
+    this.calculateTotal = this.calculateTotal.bind(this);
   }
 
-  
+  addProduct(name, quantity, price) {
+    const orders = this.state.orders;
+    let subtotal = quantity * price;
+
+    const order = {
+      name: name,
+      quantity: quantity,
+      price: price,
+      subtotal: subtotal
+    }
+
+    orders.push(order);
+
+    this.setState({orders});
+    this.calculateTotal();
+  }
+
+  calculateTotal() {
+    const orders = this.state.orders;
+    let total = 0;
+    orders.map((order) => {
+      total += order.subtotal;
+    });
+    this.setState({total: total}); 
+  } 
 
   render() {
     return (
@@ -28,43 +53,43 @@ class App extends Component {
           <div className = "row">
             <div className = "col m6">
               <Tabs>
-                <Tab title="Desayuno" className="waves-effect waves-light btn" active>
+                <Tab title="Desayuno" className="waves-effect waves-light" active>
                   <div>
                     <div className="divider"></div>
                     <h5>Cafés</h5>
-                    <Button textButton = "Café americano S/. 5" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Café con leche S/. 7" color = "light-blue darken-3"></Button>
+                    <Button textButton="Café americano S/. 5" color="light-blue darken-3" name={'Café americano'} price={5} onAddProduct={this.addProduct}></Button>
+                    <Button textButton="Café con leche S/. 7" color="light-blue darken-3" name={'Café con leche'} price={7} onAddProduct={this.addProduct}></Button>
                     <div className="divider"></div>
                     <h5>Jugos</h5>
-                    <Button textButton = "Jugo natural S/. 7" color = "light-blue darken-3"></Button>
+                    <Button textButton = "Jugo natural S/. 7" color = "light-blue darken-3" name={'Jugo natural'} price={7} onAddProduct={this.addProduct}></Button>
                     <div className="divider"></div>
                     <h5>Sandwiches</h5>
-                    <Button textButton = "Sandwich de jamón y queso S/. 7" color = "light-blue darken-3"></Button>
+                    <Button textButton = "Sandwich de jamón y queso S/. 7" color = "light-blue darken-3" name={'Sandwich de jamón y queso'} price={7} onAddProduct={this.addProduct}></Button>
                   </div>
                 </Tab>
                 <Tab title="Almuerzo / Cena">
                   <div>
                     <div className="divider"></div>
                     <h5>Hamburguesas</h5>
-                    <Button textButton = "Hamburguesa carne" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Hamburguesa pollo" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Hamburguesa vegana" color = "light-blue darken-3"></Button>
+                    <Button textButton = "Hamburguesa carne" color = "light-blue darken-3" name={'Hamburguesa carne'} price={10} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Hamburguesa pollo" color = "light-blue darken-3" name={'Hamburguesa pollo'} price={10} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Hamburguesa vegana" color = "light-blue darken-3" name={'Hamburguesa vegana'} price={10} onAddProduct={this.addProduct}></Button>
                     <div className="divider"></div>
                     <h5>Acompañamientos</h5>
-                    <Button textButton = "Papas fritas S/. 5" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Onions rings S/. 5" color = "light-blue darken-3"></Button>
+                    <Button textButton = "Papas fritas S/. 5" color = "light-blue darken-3" name={'Papas fritas'} price={5} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Onions rings S/. 5" color = "light-blue darken-3" name={'Onions rings'} price={5} onAddProduct={this.addProduct}></Button>
                     <div className="divider"></div>
                     <h5>Bebidas</h5>
-                    <Button textButton = "Agua 500 ml. S/. 5" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Gaseosa 500 ml. S/. 5" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Agua 750 ml. S/. 8" color = "light-blue darken-3"></Button>
-                    <Button textButton = "Gaseosa 750 ml. S/. 10" color = "light-blue darken-3"></Button>
+                    <Button textButton = "Agua 500 ml. S/. 5" color = "light-blue darken-3" name={'Agua 500 ml.'} price={5} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Gaseosa 500 ml. S/. 5" color = "light-blue darken-3" name={'Gaseosa 500 ml.'} price={5} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Agua 750 ml. S/. 8" color = "light-blue darken-3" name={'Agua 750 ml.'} price={8} onAddProduct={this.addProduct}></Button>
+                    <Button textButton = "Gaseosa 750 ml. S/. 10" color = "light-blue darken-3" name={'Gaseosa 750 ml.'} price={10} onAddProduct={this.addProduct}></Button>
                   </div>
                 </Tab>
               </Tabs>
             </div>
             <div className = "col m6">
-              <Form/>
+              <Form arrayOrders={this.state.orders} total={this.state.total}/>
             </div>
           </div>
         </div>
