c654a1588a62fcd01beb5893bf984a8ea70879c2
vmirella <vmirella_17@hotmail.com>
2018-11-06 06:09:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/lim-2018-01-burger-queen/precache-manifest.2241b25a400cd466d7af929ac13d2872.js"
+  "/lim-2018-01-burger-queen/precache-manifest.27a5558ab46724d3fe7eda25b9777c75.js"
 );
 
 workbox.clientsClaim();
