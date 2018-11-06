fd3ae211405738e96846e4c0c00ebfd1c3006383
vmirella <vmirella_17@hotmail.com>
2018-11-06 05:07:45 -0500

    Updates

---
+++ precache-manifest.577c6b543486004765b78fbbf04563be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "0bd9a6842376b16cebc0",
+    "url": "/lim-2018-01-burger-queen/static/js/main.0bd9a684.chunk.js"
+  },
+  {
+    "revision": "b031dfe29344e8508a4f",
+    "url": "/lim-2018-01-burger-queen/static/js/1.b031dfe2.chunk.js"
+  },
+  {
+    "revision": "0bd9a6842376b16cebc0",
+    "url": "/lim-2018-01-burger-queen/static/css/main.b323b0db.chunk.css"
+  },
+  {
+    "revision": "e89f72a9e4e13faf78c4da85723d1bcd",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
