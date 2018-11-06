573013640f2e8a7d214615d074bf79238125b2ad
vmirella <vmirella_17@hotmail.com>
2018-11-06 05:50:43 -0500

    Updates

---
+++ precache-manifest.67ad6ddbb1065b3e5d27a1b4c584be83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "95f59e5508e4139a471f",
+    "url": "/lim-2018-01-burger-queen/static/js/main.95f59e55.chunk.js"
+  },
+  {
+    "revision": "f7fbd50f43d2f7f7a2ac",
+    "url": "/lim-2018-01-burger-queen/static/js/1.f7fbd50f.chunk.js"
+  },
+  {
+    "revision": "95f59e5508e4139a471f",
+    "url": "/lim-2018-01-burger-queen/static/css/main.b323b0db.chunk.css"
+  },
+  {
+    "revision": "3dec7a612bc98a6901c84a583f5ab08a",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/lim-2018-01-burger-queen/precache-manifest.85aa18d3a56dc884786802a51edf3f78.js"
+  "/lim-2018-01-burger-queen/precache-manifest.67ad6ddbb1065b3e5d27a1b4c584be83.js"
 );
 
 workbox.clientsClaim();
